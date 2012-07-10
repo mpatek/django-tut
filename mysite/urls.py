88701556a6f36edb04190fae6c9a82655e78d4e5
Mike Patek <mpatek@gmail.com>
2012-07-10 17:24:19 -0400

    finished part 4 of tut.

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,9 +5,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^polls/$', 'polls.views.index'),
-    url(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
-    url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
-    url(r'^polls/(?P<poll_id>\d+/vote/$', 'polls.views.vote'),
-    url(r'^admin/', include(admin.site.urls)),
-)
+        url(r'^polls/', include('polls.urls')),
+        url(r'^admin/', include(admin.site.urls)),
+        )
